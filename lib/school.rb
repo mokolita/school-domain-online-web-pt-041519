3c056e214b49b0eed0e96fb74b22ea04b97dcd64
Meg Galvez <meg@oakmaine.com>
2019-04-30 15:32:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,13 @@ class School
   end 
 
   def add_student(name, grade)
+    if roster.include?(grade)
+      roster[grade] << name 
+    else 
     roster[grade] = []
     roster[grade] << name 
   end 
+end 
+
+
 end 
